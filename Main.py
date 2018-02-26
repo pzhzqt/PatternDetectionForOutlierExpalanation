1e13109b9b4cee4505b9349ba6ecd5f0eecc0422
Qitian Zeng <qzeng3@hawk.iit.edu>
2018-02-26 14:41:26 -0600

    Revert "psycopg2"
    This reverts commit 8d91c340a97c977e824694f2d5e05b9e5a80b3bd.

--- Main.py
+++ Main.py
@@ -1,24 +1,23 @@
 import sys
-import psycopg2 as pg
+import sqlalchemy as sa
 from PatternFinder import PatternFinder
 
 def main():
     #pc=PC()
     #config=input("Connection config (host port dbname username password):\n").split()
-    #config=['216.47.152.61','5432','postgres','antiprov','test']
+    config=['216.47.152.61','5432','postgres','antiprov','test']
     try:
-        #engine = sa.create_engine(
-                #'postgresql://'+config[3]+':'+config[4]+'@'
-                #+config[0]+':'+config[1]+'/'+config[2],
-                #echo=True)
-        conn=pg.connect("host='localhost' port= '5433' user='antiprov' password='antiprov' dbname='antiprov'")
+        engine = sa.create_engine(
+                'postgresql://'+config[3]+':'+config[4]+'@'
+                +config[0]+':'+config[1]+'/'+config[2],
+                echo=True)
     except Exception as ex:
         print(ex)
         sys.exit(1)
-
-    p=PatternFinder(conn)
+    
+    p=PatternFinder(engine.connect())
     p.findPattern()
-    conn.close()
+    engine.dispose()
         
 if __name__=="__main__":
     main()
